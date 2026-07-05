d61e38d513b0df3a6f471f2938262c02589fe54a
kenkingplus2016-hub <kenkingplus2016@gmail.com>
2026-07-05 13:57:55 +0100

    Update Crying Tiger menu

---
+++ replace.js
@@ -0,0 +1,4 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('./data/classic_menu.json', 'utf8');
+content = content.replace(/Nam Tok Suea Rong Hai/g, 'Nam Tok Crying Tiger');
+fs.writeFileSync('./data/classic_menu.json', content, 'utf8');
