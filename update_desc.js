81e1d4f0f9da1598d439a02c30285805530edf33
kenkingplus2016-hub <kenkingplus2016@gmail.com>
2026-07-05 14:43:25 +0100

    Update Hat Yai Chicken description

---
+++ update_desc.js
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+let data = JSON.parse(fs.readFileSync('./data/classic_menu.json', 'utf8'));
+
+let item = data.find(d => d.id == 102);
+if (item) {
+    item.desc.th = "ไก่ทอดหาดใหญ่ น้ำอาจาด น้ำจิ้มไก่ หอมเจียว";
+    item.desc.en = "Hat Yai Fried Chicken, Ajad Relish, Sweet Chili Sauce, Fried Shallots";
+}
+
+fs.writeFileSync('./data/classic_menu.json', JSON.stringify(data, null, 2), 'utf8');
